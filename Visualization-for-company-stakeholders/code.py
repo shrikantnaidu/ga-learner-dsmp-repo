eaf61600202433a587402b7f8a9b9b4623560d99
thelazyscripter777 <thelazyscripter777@users.noreply.github.com>
2019-08-08 18:59:29 +0530

    Code file created.

---
+++ Visualization-for-company-stakeholders/code.py
@@ -0,0 +1,96 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv(path)
+loan_status = data['Loan_Status'].value_counts()
+print(loan_status)
+loan_status.plot(kind='bar') 
+plt.show()
+
+#Code starts here
+
+
+# --------------
+#Code starts here
+property_and_loan = data.groupby(['Property_Area','Loan_Status'])
+
+property_and_loan = property_and_loan.size().unstack()
+print(property_and_loan.head(10))
+property_and_loan.plot(kind='bar', stacked=False, figsize=(15,10))
+plt.xlabel('Property Area')
+plt.ylabel('Loan Status')
+plt.show()
+
+
+# --------------
+#Code starts here
+education_and_loan =data.groupby(['Education','Loan_Status'])
+education_and_loan = education_and_loan.size().unstack()
+education_and_loan.plot(kind='bar', stacked=True)
+plt.xlabel('Education Status')
+plt.ylabel('Loan Status')
+plt.show()
+
+
+
+# --------------
+#Code starts here
+graduate = pd.DataFrame(data[data['Education']=='Graduate'])
+not_graduate = pd.DataFrame(data[data['Education']=='Not Graduate'])
+graduate['LoanAmount'].plot(kind='density')
+not_graduate['LoanAmount'].plot(kind='density')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+
+#Setting up the subplots
+fig, (ax_1, ax_2,ax_3) = plt.subplots(1,3, figsize=(20,8))
+
+#Plotting scatter plot
+ax_1.scatter(data['ApplicantIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_1.set(title='Applicant Income')
+
+
+#Plotting scatter plot
+ax_2.scatter(data['CoapplicantIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_2.set(title='Coapplicant Income')
+
+
+#Creating a new column 'TotalIncome'
+data['TotalIncome']= data['ApplicantIncome']+ data['CoapplicantIncome']
+
+#Plotting scatter plot
+ax_3.scatter(data['TotalIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_3.set(title='Total Income')
+
+
+#Code ends here
+
+
